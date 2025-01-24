8c158ea1c8a1576258b097f443e36501eb100fa5
cachedpotato <chiwonchung98@gmail.com>
2025-01-25 00:10:28 +0900

    more practice with binary search questions

--- 04_binary_search/practice/binary_search.rs
+++ 04_binary_search/practice/binary_search.rs
@@ -1,6 +1,6 @@
 fn binary_search_iter(nums: &Vec<i32>, target: i32) -> i32 {
     let mut l = 0;
-    let mut r = 0;
+    let mut r = nums.len() - 1;
 
     while l <= r {
         let m = l + (r - l) / 2;
@@ -17,25 +17,40 @@ fn binary_search_iter(nums: &Vec<i32>, target: i32) -> i32 {
 
 fn binary_search_lower_bound(nums: &Vec<i32>, target: i32) -> i32 {
     let mut l = 0;
-    let mut r = 0;
+    let mut r = nums.len();
 
-    while l <= r {
+    while l < r { //if using bound approach: l < r / if iterative: l <= r
         let m = l + (r - l) / 2;
+        if nums[m] >= target {
+            r = m;
+        } else {
+            l = m + 1;
+        }
     }
+
+    if l < nums.len() && nums[l] == target {return l as i32;} else {return -1;}
 }
 
 
 fn binary_search_upper_bound(nums: &Vec<i32>, target: i32) -> i32 {
     let mut l = 0;
-    let mut r = 0;
+    let mut r = nums.len();
 
-    while l <= r {
+    while l < r {
         let m = l + (r - l) / 2;
+        if nums[m] > target {
+            r = m;
+        } else {
+            l = m + 1;
+        }
     }
+
+    if l > 0 && nums[l - 1] == target {return (l - 1) as i32;} else {return -1;}
 }
 
 fn main() {
     let nums = vec![1, 2, 5, 8, 10];
-    println!("{}", binary_search_iter(&nums, 4))
-
+    println!("{}", binary_search_iter(&nums, 8));
+    println!("{}", binary_search_upper_bound(&nums, 8));
+    println!("{}", binary_search_lower_bound(&nums, 10));
 }
