541d413be763d275ca31ed125df87dd1c60d22cc
cachedpotato <chiwonchung98@gmail.com>
2025-02-04 15:35:49 +0900

    linked list kinda done

--- 0600_linked_list/0601_reverse_linked_list/src/main.rs
+++ 0600_linked_list/0601_reverse_linked_list/src/main.rs
@@ -1,3 +1,13 @@
+use rust_linked_list::sixth::LinkedList;
 fn main() {
-    println!("Hello, world!");
+    let mut list = LinkedList::from_iter(vec![1, 2, 3, 4]);
+    list.reverse();
+    println!("{:?}", list);
+
+    let another = LinkedList::from_iter(vec![1, 2]);
+    println!("{:?}", LinkedList::reverse_list(another));
+
+    let mut empty: LinkedList<i32> = LinkedList::new();
+    empty.reverse();
+    println!("{:?}", empty);
 }
